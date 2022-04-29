47d9605444dbd751de8ddff2bc46cb5578f9fee5
shubhadapaithankar <shubhadasanjay.paithankar@sjsu.edu>
2022-04-28 22:36:45 -0700

    back image has been set

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,28 @@
 import './App.css';
 import Appbar from './components/Appbar'
 import Student from './components/Student'
+import backimage from "./images/back.jpg";
 
 function App() {
   return (
+
+    <div
+    class="bg_image"
+    style={{
+      backgroundImage: 'url('+backlimage+')',
+      backgroundSize: "cover",
+      height: "280vh",
+      color: "#f5f5f5",
+      
+      backgroundRepeat: 'no-repeat'
+    }}
+  >
+
     <div className="App">
     <Appbar/>
     <Student/>
    
+    </div>
     </div>
   );
 }
