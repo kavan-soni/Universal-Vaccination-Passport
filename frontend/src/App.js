6eee236c86751f56cc32e007a6eea0bcaa02b08d
shubhadapaithankar <99461999+shubhadapaithankar@users.noreply.github.com>
2022-04-28 23:00:12 -0700

    Update App.js
    changes applied

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,7 @@ function App() {
     style={{
       backgroundImage: 'url('+backimage+')',
       backgroundSize: "cover",
-      height: "280vh",
+      height: "200vh",
       color: "#f5f5f5",
       
       backgroundRepeat: 'no-repeat'
